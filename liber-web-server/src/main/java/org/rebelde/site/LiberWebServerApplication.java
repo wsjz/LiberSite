a088178f4ed7330bf9bdd3a0e508efe4999c44d8
rebelde <18522955+wsjz@users.noreply.github.com>
2025-06-20 16:03:26 +0800

    Initial commit

---
+++ liber-web-server/src/main/java/org/rebelde/site/LiberWebServerApplication.java
@@ -0,0 +1,12 @@
+package org.rebelde.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberWebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiberWebServerApplication.class, args);
+    }
+}
---
+++ liber-web-server/src/test/java/org/rebelde/site/LiberWebServerApplicationTests.java
@@ -0,0 +1,12 @@
+package org.rebelde.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberWebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
